87b28c602a8019a6285d00bb20f9824462965867
Jorge Padrón <jorge-padron99@otmail.com>
2022-04-26 19:25:15 +0200

    Add Firesotre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input'; 
-import {MatButtonModule} from '@angular/material/button'; 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button'; 
 import { MatFormFieldModule } from '@angular/material/form-field'; 
-import {MatListModule} from '@angular/material/list'; 
-import {MatCardModule} from '@angular/material/card'; 
+import { MatListModule } from '@angular/material/list'; 
+import { MatCardModule } from '@angular/material/card'; 
 
+import { environment } from 'src/environments/environment';
+import { provideFirestore,getFirestore,connectFirestoreEmulator  } from '@angular/fire/firestore';
+import { initializeApp,provideFirebaseApp, getApp } from '@angular/fire/app';
+
+import {MatDatepickerModule} from '@angular/material/datepicker'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -34,6 +39,9 @@ import { TodoUpdateComponent } from './todo-update/todo-update.component';
     MatInputModule,
     FormsModule,
     MatCardModule,
+    MatDatepickerModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
