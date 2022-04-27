7a3e433e8c8fd52b3822ce19c7353653e5da850d
Jorge Padrón <jorge-padron99@otmail.com>
2022-04-27 19:27:45 +0200

    Mas cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC8vEdffXC12XE037YERC37sbXjL5KshMI",
+    authDomain: "todoproject-45555.firebaseapp.com",
+    projectId: "todoproject-45555",
+    storageBucket: "todoproject-45555.appspot.com",
+    messagingSenderId: "545346151723",
+    appId: "1:545346151723:web:29872ba24b5d219301916e",
+    measurementId: "G-E228QVH0DG"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,21 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-
     apiKey: "AIzaSyC8vEdffXC12XE037YERC37sbXjL5KshMI",
-  
     authDomain: "todoproject-45555.firebaseapp.com",
-  
     projectId: "todoproject-45555",
-  
     storageBucket: "todoproject-45555.appspot.com",
-  
     messagingSenderId: "545346151723",
-  
     appId: "1:545346151723:web:29872ba24b5d219301916e",
-  
     measurementId: "G-E228QVH0DG"
-  
   }
   
 };
